76c47674ce1ecff661c0b2770ea96e7785d5d12f
Evans Musomi <evans.musomi@gmail.com>
2018-04-01 06:02:54 +0300

    Adds Heroku Required Database Connection String and Env Vars

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const signin = require("./controllers/signin");
 const db = knex({
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.SSL
   }
 });
 
